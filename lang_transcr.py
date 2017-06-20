ecfb70dd6901ce67f78b6228a6e391f82786f9df
az-song <alexzsong@gmail.com>
2017-06-20 13:43:54 -0400

    Update lang_transcr.py

--- lang_transcr.py
+++ lang_transcr.py
@@ -82,19 +82,22 @@ for file in lang_files:  # Iterate through every found excel file
 
             if ('1.' in trans_clear.loc[i]['response']) == True:
                 transcription[0] = lang_trans.at[i, 'response']
-            
+
             if ('2.' in trans_clear.loc[i]['response']) == True:
                 transcription[1] = lang_trans.at[i, 'response']
-            
+
             if ('3.' in trans_clear.loc[i]['response']) == True:
                 transcription[2] = lang_trans.at[i, 'response']
-            
+
             if ('4.' in trans_clear.loc[i]['response']) == True:
                 transcription[3] = lang_trans.at[i, 'response']
-            
+
             if ('5.' in trans_clear.loc[i]['response']) == True:
                 transcription[4] = lang_trans.at[i, 'response']
-                
+
+            if ('6.' in trans_clear.loc[i]['response']) == True:
+                response6 = lang_trans.at[i, 'response']                
+
             trans_df = pd.DataFrame(data = [transcription], columns = [col for col in cols.columns if 'lang_transcr_' in col])
 
             single_test = pd.concat([single_test, trans_df], axis=1)
