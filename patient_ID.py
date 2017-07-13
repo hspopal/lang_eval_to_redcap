9517bb7ea923857ee09eebd7242a628ae250e689
az-song <alexzsong@gmail.com>
2017-07-13 12:28:24 -0400

    Update patient_ID.py

--- patient_ID.py
+++ patient_ID.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import csv
 import numpy as np
@@ -5,6 +6,7 @@ import re
 
 import os
 import fnmatch
+from datetime import datetime
 
 # Capture all subject lang files
 # Skip spreadsheets that have errors for missing tabs, or ill formatted headers
@@ -24,39 +26,48 @@ def find(pattern, path):
     return result
 
 lang_files = find('*.xls', work_dir + '/Patients/')
-# lang_files = [work_dir +
-# '/Patients/LastNameA_F/Adamian_Daniel'
-# '/010815/adamian_lang_010815.xls']
+#lang_files = [work_dir + '/Patients/LastNameA_F/Adamian_Daniel/010815/adamian_lang_010815.xls']
+#lang_files = [work_dir + '/Patients/LastNameA_F/Blake, Franklin Howard/022015/lang_FB.xls']
 
 data = []
 
 # cols will be used to build dataframe off of specific Redcap headers
 cols = pd.read_csv(work_dir + '/redcap_headers.csv')
 
-single_test = pd.DataFrame()
+ID = pd.DataFrame()
 count = 0
 
-missing_bnt30 = []
-missing_wab_commands = []
-missing_wab_repetition = []
-missing_wab_reading = []
-
-header_error_bnt30 = []
-header_error_wab_reading = []
-
-all_test = pd.DataFrame()
-
-raw_ID = []
+date_error = []
 
 ID_error_firstname = []
 ID_error_lastname = []
 ID_error_date = []
+ID_error = []
+
+# Load Redcap csv as a dataframe
+
+redcap_df = pd.read_csv(work_dir + '/DickersonMasterEnrol_DATA_2017-07-12_1344.csv')
+
+# Create a column for IDs in Redcap df that match lang spreadsheet IDs (can just be first and last name)
+
+# pull only relevant columns
+## redcap_id, event, bnt30, wab, etc.
+
+bnt30_col = []
+lang_col = []
+wab_rep_col = []
+
+relevant_columns = ['subject_id', 'Subject', 'redcap_event_name', 'name_last']
+
+redcap_relevant = redcap_df.iloc[:, 0:8]
+
+redcap_relevant['Subject'] = redcap_relevant["name_last"].map(str) + '_' + redcap_relevant["name_first"]
 
 for file in lang_files:  # Iterate through every found excel file
-    single_test = pd.DataFrame()
+    single_ID = pd.DataFrame()
 
     # Find subject's name from file path
-    single_test['Subject'] = []
+    single_ID['Subject'] = []
     m = re.search(work_dir + '/Patients/LastNameA_F/(.+?)/', file)
     if m:
         found = m.group(1)
@@ -66,32 +77,71 @@ for file in lang_files:  # Iterate through every found excel file
     m = re.search(work_dir + '/Patients/LastNameN_Z/(.+?)/', file)
     if m:
         found = m.group(1)
-    single_test.ix[0, 'Subject'] = found
-
-    xl = pd.ExcelFile(file)
-    sprdshts = xl.sheet_names
-
-    date = str(file[-10:-4])
-
-    raw = str(file[67:])
-    raw_ID.append(raw)
-
-ID = pd.DataFrame(raw_ID, columns=['raw'])
-ID['first_name'] = ID['raw'].str.extract('/.+?_(.*)/\d', expand=True)
-if ID['first_name'].isnull:
-    ID_error_firstname.append(file)
-else:
-    ID['first_initial'] = ID['first_name'].astype(str).str[0]
-    ID['last_name'] = ID['raw'].str.extract('/(.*)_.+?/\d', expand=True)
-    if ID['last_name'].isnull:
-        ID_error_lastname.append(file)
-    else:
-        ID['date'] = ID['raw'].str.extract('/(\d\d\d\d\d\d)/', expand=True)
-        if ID['date'].isnull:
-            ID['date'] = str(ID['raw'].astype(str).str[-10:-4])
-            if ID['date'].isnull:
-                ID_error_date.append(file)
+    single_ID.ix[0, 'Subject'] = found
+    
+    match = re.search(r'/(\d\d\d\d\d\d)/', file)
+    if match is None:
+        match = re.search(r'(\d\d\d\d\d\d)/', file)
+        if match is None:
+            match = re.search(r'(\d\d\d\d\d\d).xls', file)
+            if match is None:
+                match = re.search(r'(\d\d_\d\d_\d\d)', file)
+                if match is None:
+                    match = re.search(r'(\d\d.\d\d.\d\d)', file)
+                    if match is None:
+                        match = re.search(r'_(\d\d\d\d\d\d)', file)
+                        if match is None:
+                            date_error.append(file)
+                            single_ID.ix[0, 'Date'] = ''
+                        else:
+                            date = datetime.strptime((match.group())[1:], '%m%d%y').date()
+                            single_ID.ix[0, 'Date'] = str(date)
+                    else:
+                        date = datetime.strptime((match.group()), '%m.%d.%y').date()
+                        single_ID.ix[0, 'Date'] = str(date)
+                else:
+                    date = datetime.strptime((match.group()), '%m_%d_%y').date()
+                    single_ID.ix[0, 'Date'] = str(date)
+            else:
+                date = datetime.strptime((match.group())[:-4], '%m%d%y').date()
+                single_ID.ix[0, 'Date'] = str(date)
         else:
-            ID_lower = ID['first_initial'] + (ID['last_name'].astype
-                                              (str).str[:3] + '_')
-            ID['ID'] = ID_lower.str.upper()
+            date = datetime.strptime((match.group())[:-1], '%m%d%y').date()
+            single_ID.ix[0, 'Date'] = str(date)
+    else:
+        date = datetime.strptime((match.group())[1:-1], '%m%d%y').date()
+        single_ID.ix[0, 'Date'] = str(date)
+        
+    ID = ID.append(single_ID)
+
+ID['name_first'] = ID['Subject'].str.extract('.+?_(.*)', expand=True)
+#if(pd.isnull(ID['name_first'])):
+#    ID['name_first'] = ID['Subject'].str.extract('.+?, (.*)', expand=True)
+ID['First_Initial'] = ID['name_first'].astype(str).str[0]
+ID['name_last'] = ID['Subject'].str.extract('(.*)_.+?', expand=True)
+#if ID['name_last'].isnull().bool == True:
+#    ID['name_last'] = ID['Subject'].str.extract('(.*), .+?', expand=True)
+
+ID_lower = ID['First_Initial'] + (ID['name_last'].astype
+                                  (str).str[:3]) + '_'
+ID['ID'] = ID_lower.str.upper()
+
+#if ID['Date'].isnull().bool == True:
+#    ID_error_date.append(file)
+
+#if ID['name_first'].isnull().bool == True:
+#    ID_error_firstname.append(file)
+#if ID['name_last'].isnull().bool == True:
+#    ID_error_lastname.append(file)
+
+redcap_relevant.set_index('Subject',inplace=True)
+redcap_relevant_map = redcap_relevant.iloc[:,0].to_dict()
+ID['PatientID']=ID.Subject.map(redcap_relevant_map)
+
+error = ID['PatientID'].isnull().sum()
+
+ID.to_csv('PatientID.csv')
+
+#want pull data from redcap master['ï»¿subject_id'] based on same from name_last
+# maps values from redcap.ï»¿subject_id based on values in redcap name_last
+#column to t1 that simply maps values from t2.target based on the values in t2.letters
